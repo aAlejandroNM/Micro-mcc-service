62e89b1ff0ecc3db4071134e787414e92f870635
aAlejandroNM <adrian.neira@uptc.edu.co>
2025-05-18 21:36:32 -0500

    Primer commit

---
+++ src/test/java/com/dante/mcc_customer_service/MccCustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dante.mcc_customer_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MccCustomerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
